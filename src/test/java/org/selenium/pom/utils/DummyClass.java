735306e05d270bfebdb1c4e7db8d5fb1b78dc357
pushkar-jadhav <pushkarjadhav87@gmail.com>
2024-08-12 19:46:19 -0400

    dummy code added

--- src/test/java/org/selenium/pom/utils/DummyClass.java
+++ src/test/java/org/selenium/pom/utils/DummyClass.java
@@ -1,4 +1,11 @@
 package org.selenium.pom.utils;
 
+import org.testng.annotations.Test;
+
 public class DummyClass {
+
+    @Test
+    public void dummyMethod(){
+     System.out.println("This is dummy method to validate git webhook");
+    }
 }
